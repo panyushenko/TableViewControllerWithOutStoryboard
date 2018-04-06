8d2a834a88e22c448e04667aed93f8f50398354c
Panyushenko <>
2018-04-06 13:39:27 +0300

    deleted ViewController and MainStoryboard created new tableViewController, tableViewCell and its XIB file edited AppDelegate(create UINavigationController and UIWindow without Storyboard) edited Cell (TableViewCell.xib)

---
+++ TableViewControllerWithOutStoryboard/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableViewControllerWithOutStoryboard
+//
+//  Created by Panyushenko on 06.04.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TableViewControllerWithOutStoryboard/TableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TableViewController.swift
+//  TableViewControllerWithOutStoryboard
+//
+//  Created by Panyushenko on 06.04.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "reuseIdentifier")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+
+        return cell
+    }
+}
